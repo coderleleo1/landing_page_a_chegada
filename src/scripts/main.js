76cec431faacdef6638f49aeadde2b984b371825
Leonardo <leobortoli2005@gmail.com>
2025-05-03 20:50:38 -0300

    finalização do movie, alguns novos ajustes pendentes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-document.addEventListener('DOMContentLoaded',)
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao){
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            escondeTodasAbas();
+            aba.classList.add(`movie__images--is-active`)
+            removeBotaoAtivo();
+            botao.target.classList.add(`movie__tabs__button--is-active`)
+        })
+    }
+})
+
+function escondeTodasAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove(`movie__images--is-active`)
+    }
+}
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]')
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove(`movie__tabs__button--is-active`)
+    }
+}
