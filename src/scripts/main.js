b89de6bf0fa78cee3289c1f492e291e756b50adc
Leonardo <leobortoli2005@gmail.com>
2025-05-09 17:31:01 -0300

    finalização do projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('[data-tab-button]');
+    const buttons = document.querySelectorAll('[data-tab-button]'); // movie
+    const questions = document.querySelectorAll('[data-faq-question]'); // FAQ
 
+    const heroSection = document.querySelector('.hero'); // header
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() { // comportamento header
+        const posicaoAtual = this.window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    // configuração da section _movie
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -11,7 +26,24 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add(`movie__tabs__button--is-active`)
         })
     }
+
+    // configuração do FAQ
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+// configuração da section _movie
 
 function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
@@ -27,4 +59,13 @@ function removeBotaoAtivo() {
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove(`movie__tabs__button--is-active`)
     }
+}
+
+// configuração do FAQ
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
 }
